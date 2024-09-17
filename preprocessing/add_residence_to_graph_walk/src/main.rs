fc1b9cd69fb6f064903a7956b1fa4e660c48705e
ADD-William-WaltersDavis <william.waltersdavis@dft.gov.uk>
2024-09-17 12:37:17 +0100

    fix: add pt route nodes as well as the walking nodes

--- preprocessing/add_residence_to_graph_walk/src/main.rs
+++ preprocessing/add_residence_to_graph_walk/src/main.rs
@@ -1,5 +1,5 @@
-use common::common::{NodeWalk, NodeWalkWeighted, write_json_file};
 use anyhow::Result;
+use common::common::{write_json_file, NodeWalk, NodeWalkWeighted};
 use fs_err::File;
 use std::io::BufReader;
 
@@ -8,8 +8,11 @@ fn main() {
     let residence_node_weightings = read_residence_node_weightings().unwrap();
 
     let mut pt_graph_walk_weighted: Vec<NodeWalkWeighted> = Vec::new();
-    for (i, node) in pt_graph_walk.iter().enumerate() {
+
+    // push pt walking nodes with residence weightings
+    for i in 0..residence_node_weightings.len() {
         let weight = residence_node_weightings[i];
+        let node = &pt_graph_walk[i];
         let has_residence = weight != 0;
         let node_walk_weighted = NodeWalkWeighted {
             has_pt: node.has_pt,
@@ -19,7 +22,26 @@ fn main() {
         };
         pt_graph_walk_weighted.push(node_walk_weighted);
     }
-    write_json_file(format!("pt_graph_walk_weighted"), "../data", pt_graph_walk_weighted).unwrap();
+    // push the pt route nodes with no residences
+    for i in residence_node_weightings.len()..pt_graph_walk.len() {
+        let node = &pt_graph_walk[i];
+        let node_walk_weighted = NodeWalkWeighted {
+            has_pt: node.has_pt,
+            edges: node.edges.clone(),
+            has_residence: false,
+            weight: 0,
+        };
+        pt_graph_walk_weighted.push(node_walk_weighted);
+    }
+
+    assert_eq!(pt_graph_walk.len(), pt_graph_walk_weighted.len());
+
+    write_json_file(
+        format!("pt_graph_walk_weighted"),
+        "../data",
+        pt_graph_walk_weighted,
+    )
+    .unwrap();
 }
 
 fn read_pt_graph_walk() -> Result<Vec<NodeWalk>> {
